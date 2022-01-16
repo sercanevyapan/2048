bb5f90ef8c2204e29030f0cd9d470208dd707ad7
Sercan Evyapan <sevyapan@gmail.com>
2022-01-16 18:19:10 +0300

    Finish Game

--- Assets/_Scripts/Block.cs
+++ Assets/_Scripts/Block.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     public int Value;
+    public Node Node;
+    public Block MergingBlock;
+    public bool Merging;
+    public Vector2 Pos => transform.position;
     [SerializeField] private SpriteRenderer _renderer;
     [SerializeField] private TextMeshPro _text;
     public void Init(BlockType type)
@@ -14,4 +18,22 @@ public class Block : MonoBehaviour
         _renderer.color = type.Color;
         _text.text = type.Value.ToString();
     }
+
+    public void SetBlock(Node node)
+    {
+        if (Node != null) Node.OccupiedBlock = null;
+        Node = node;
+        Node.OccupiedBlock = this;
+    }
+
+    public void MergeBlock(Block blockToMergeWith)
+    {
+        MergingBlock = blockToMergeWith;
+
+        Node.OccupiedBlock = null;
+
+        blockToMergeWith.Merging = true;
+    }
+
+    public bool CanMerge(int value) => value == Value && !Merging && MergingBlock == null;
 }
--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -13,6 +14,10 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Block _blockPrefab;
     [SerializeField] private SpriteRenderer _boardPrefab;
     [SerializeField] private List<BlockType> _types;
+    [SerializeField] private float _travelTime = 0.2f;
+    [SerializeField] private int _winCondition = 2048;
+
+    [SerializeField] private GameObject _winScreen, _loseScreen;
 
     private List<Node> _nodes;
     private List<Block> _blocks;
@@ -43,18 +48,31 @@ public class GameManager : MonoBehaviour
             case GameState.Moving:
                 break;
             case GameState.Win:
+                _winScreen.SetActive(true);
                 break;
             case GameState.Lose:
+                _loseScreen.SetActive(true);
                 break;
             default:
                 throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
         }
     }
 
+    private void Update()
+    {
+        if (_state != GameState.WaitingInput) return;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) Shift(Vector2.left);
+        if (Input.GetKeyDown(KeyCode.RightArrow)) Shift(Vector2.right);
+        if (Input.GetKeyDown(KeyCode.UpArrow)) Shift(Vector2.up);
+        if (Input.GetKeyDown(KeyCode.DownArrow)) Shift(Vector2.down);
+    }
+
     void GenerateGrid()
     {
         _round = 0;
         _nodes = new List<Node>();
+        _blocks = new List<Block>();
         for (int x = 0; x < _width; x++)
         {
             for (int y = 0; y < _height; y++)
@@ -79,20 +97,103 @@ public class GameManager : MonoBehaviour
 
         foreach (var node in freeNodes.Take(amount))
         {
-            var block = Instantiate(_blockPrefab,node.Pos, Quaternion.identity);
-            block.Init(GetBlockTypeByValue(Random.value > 0.8f ? 4 :2 ));
+            SpawnBlock(node, Random.value > 0.8f ? 4 : 2);
         }
        
 
 
         if (freeNodes.Count() == 1)
         {
-            //Lost the game
+            ChangeState(GameState.Lose);
             return;
         }
+
+        ChangeState(_blocks.Any(b=> b.Value == _winCondition) ? GameState.Win : GameState.WaitingInput);
     }
 
+    void SpawnBlock(Node node, int value)
+    {
+        var block = Instantiate(_blockPrefab, node.Pos, Quaternion.identity);
+        block.Init(GetBlockTypeByValue(value));
+        block.SetBlock(node);
+        _blocks.Add(block);
+    }
+
+    void Shift(Vector2 dir)
+    {
+        ChangeState(GameState.Moving);
+
+        var orderBlocks = _blocks.OrderBy(b => b.Pos.x).ThenBy(b => b.Pos.y).ToList();
+        if (dir == Vector2.right || dir == Vector2.up) orderBlocks.Reverse();
+
+        foreach (var block in orderBlocks)
+        {
+            var next = block.Node;
+            do
+            {
+                block.SetBlock(next);
+
+                var possibleNode = GetNodeAtPosition(next.Pos + dir);
+                if(possibleNode != null)
+                {
+                    if(possibleNode.OccupiedBlock !=null && possibleNode.OccupiedBlock.CanMerge(block.Value))
+                    {
+                        block.MergeBlock(possibleNode.OccupiedBlock);
+
+                    }
+
+                    else if (possibleNode.OccupiedBlock == null) next = possibleNode;
+                }
+
+
+
+            } while (next != block.Node);
+
+
+            
+        }
+
+
+        var sequence = DOTween.Sequence();
+
+        foreach (var block in orderBlocks)
+        {
+            var movePoint = block.MergingBlock != null ? block.MergingBlock.Node.Pos : block.Node.Pos;
+
+            sequence.Insert(0, block.transform.DOMove(movePoint, _travelTime));
+
+        }
+
+        sequence.OnComplete(() =>
+        {
+            foreach (var block in orderBlocks.Where(b=> b.MergingBlock !=null))
+            {
+                MergeBlocks(block.MergingBlock, block );
+            }
+
+            ChangeState(GameState.SpawingBlocks);
+        });
+
+    }
+
+    void MergeBlocks(Block baseBlock, Block merginBlock)
+    {
+        SpawnBlock(baseBlock.Node, baseBlock.Value *2);
+
+        RemoveBlock(baseBlock);
+        RemoveBlock(merginBlock);
+    }
    
+    void RemoveBlock(Block block)
+    {
+        _blocks.Remove(block);
+        Destroy(block.gameObject);
+    }
+
+    Node GetNodeAtPosition(Vector2 pos)
+    {
+        return _nodes.FirstOrDefault(n => n.Pos == pos);
+    }
 }
 
 
